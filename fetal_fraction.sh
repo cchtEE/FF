e6bd55b57ee145b409b395e64caca258eaeb9619
Priit Paluoja <priitpaluoja@gmail.com>
2018-05-26 10:33:44 +0300

    Move to SAM only

--- fetal_fraction.sh
+++ fetal_fraction.sh
@@ -2,31 +2,45 @@
 #SBATCH --time=08:30:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=priitpaluoja@gmail.com
-#SBATCH --mem=14000
-#SBATCH -J SeqFFMM
+#SBATCH --mem=25000
+#SBATCH -J SeqSAM
 # The job requires 1 compute node
 #SBATCH -N 1
 # The job requires 1 task per node
 #SBATCH --ntasks-per-node=1
 # Number of CPU cores per task
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=40
 
-module load samtools-0.1.19
-module load python-3.6.3
+# Bash script authors: Priit Paluoja, Priit Palta, Hindrek Teder, Kaarel Krjutshkov. (CCHT)
+#
+# Usage:
+# 1. Provide system with R and Anaconda with source "ff" which contains bowtie2 and samtools. (conda install bowtie2 and conda install samtools)
+# 2. Download and modify seqFF to accept piped sam file.
+# 3. Download bowtie2 hg19 pre-built index file and update bash script with corresponding location
+# 4. Fill file "locations" with folder paths to samples. All sample X files must be in one folder. Locations can contain any number of samples.
+# 5. Run the script. Last tested on Rocket cluster (rocket.hpc.ut.ee) on 26.05.2018.
+
+
 module load R-3.2.0
+module load python-3.6.3
 
-# Build index
-# bowtie2-build GCA_000001405.15_GRCh38_genomic.fna bwtie38
-SOFTBWT=/gpfs/hpchome/ppaluoja/software/bowtie2-2.3.3.1/bowtie2
-REF=/gpfs/rocket/samba/CCHT/BelgiaNIPT/fastq/bwtie38
+source activate ff # Source most provide bowtie2 and samtools
+
+# Pre-built Bowtie 2 index (must be GRCh37/hg19)
+REF=/gpfs/hpchome/ppaluoja/software/databases/hg19
+# File which provides path(s) to fastq/fastq.gz files. For each sample, all the sample files must be in the same directory. Locations file can contain multiple samples.
 locations="locations"
 
 # Read locations from file and calculate FF.
+# 1. Concatenate fastq
+# 2. Map
+# 3. Filter
+# 4. Calculate FF
 while IFS='' read -r line || [[ -n "$line" ]]; do
     location=$line
     sample=${location##*/}
     echo $sample
-    zcat --force $location/*.fastq* | $SOFTBWT --very-sensitive -X 500 -q - --norc -x $REF --no-unal -p 10 --mm | samtools view -q 10 -S - | python3 separator.py $sample "50000" | Rscript seqff.R >> results.all.10.ff.tsv
+    zcat --force $location/*.fastq* | bowtie2 --very-sensitive -X 500 -q - --norc -x $REF --no-unal -p 40 | samtools view -q 0 -S - | cut -f3,4 | Rscript seqff.sam.R | (printf "$sample\t" && cat) >> results.all.hg19.hg19.q0.ff.sam.tsv
 done < "$locations"
 
 echo "DONE!"
